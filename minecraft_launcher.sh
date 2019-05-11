22cb11d5749df8a15cd637b5d44f547d4d2002bb
teflontoni <torben@mohrfeldt.net>
2019-05-11 08:47:53 +0200

    Some cleaning...

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -5,9 +5,6 @@ base_dir="/var/games"
 work_dir="${base_dir}"/"${server}"
 
 mkdir -p "${work_dir}"
-touch "${work_dir}"/whitelist.json
-touch "${work_dir}"/permissions.json
-touch "${work_dir}"/server.properties
 
 docker run -d -h ${server} --name=${server}\
  -v "${work_dir}:/opt/minecraft:z"\
