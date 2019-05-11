7bc623a170334c413cceea7e286c8b8f25e6cacf
teflontoni <torben@mohrfeldt.net>
2019-05-11 09:02:25 +0200

    Derpage?
    most likely

--- minecraft_launcher.sh
+++ minecraft_launcher.sh
@@ -5,8 +5,14 @@ base_dir="/var/games"
 work_dir="${base_dir}"/"${server}"
 
 mkdir -p "${work_dir}"
+touch "${work_dir}"/whitelist.json
+touch "${work_dir}"/server.properties
+touch "${work_dir}"/permissions.json
 
 docker run -d -h ${server} --name=${server}\
- -v "${work_dir}:/opt/minecraft:z"\
+ -v "${work_dir}/whitelist.json:/opt/minecraft/whitelist.json:z"\
+ -v "${work_dir}/server.properties:/opt/minecraft/server.properties:z"\
+ -v "${work_dir}/ops.json:/opt/minecraft/permissions.json:z"\
+ -v "${work_dir}/worlds:/opt/minecraft/worlds:z"\
  --network=host\
  teflontoni/minecraft-bedrockserver
